79208b58a608aaf1a0b7fb3c250c091938c9aa18
Ayush <ayushbahuguna006@gmail.com>
2026-04-17 12:08:45 +0530

    [Frontend] navbar layout updated

---
+++ Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <nav className='flex justify-center gap-2 md:gap-10 p-2 font-bold mt-5 bg-gradient-to-r from-[#1a1a1a]/80 via-[#2a2a2a]/70 to-[#1a1a1a]/80 
+      backdrop-blur-md rounded-full md:mx-[20vw] border border-white/10  text-white shadow-lg '>
+
+        <p className='hidden md:block translate-y-3'>Home</p>
+        <div className='flex-col justify-start'>
+          <p className='text-center text-xl'>VSUDHΛRΛ</p>
+          <p >Every life matters</p>
+        </div>
+        <p className='hidden md:block translate-y-3'>About</p>
+        <p className='hidden md:block translate-y-3'>Contact Us</p>
+
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
