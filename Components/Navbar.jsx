8818dd39cfe48416c4688614d71012773aebcedb
Ayush <ayushbahuguna006@gmail.com>
2026-04-18 17:54:48 +0530

    [FRONTEND] Debug bg-color bug

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom"
  
 function Navbar() {
   return (
-    <>
-      <nav className='flex justify-around gap-2 md:gap-10 p-2 font-bold mt-5 bg-gradient-to-r from-[#1a1a1a]/80 via-[#2a2a2a]/70 to-[#1a1a1a]/80 
-      backdrop-blur-md rounded-full md:mx-[20vw] border border-white/10  text-white shadow-lg mx-2'>
+    <div className="pt-5 ">
+      <nav className='flex justify-around gap-2 md:gap-10 p-2 font-bold bg-gradient-to-r from-[#1a1a1a]/80 via-[#2a2a2a]/70 to-[#1a1a1a]/80 
+      backdrop-blur-md rounded-full md:mx-[20vw] border border-white/10 text-white shadow-lg mx-2'>
 
         <NavLink to="/" className='hidden md:block translate-y-3'>Home</NavLink>
 
@@ -19,7 +19,7 @@ function Navbar() {
         <NavLink to="/contact" className='hidden md:block translate-y-3'>Contact Us</NavLink>
 
       </nav>
-    </>
+    </div>
   )
 }
 
