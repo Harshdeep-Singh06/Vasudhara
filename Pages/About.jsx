9cc351926e84a06a1381a3649a9bdf70d76a263c
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-04-22 14:33:07 +0530

    [Frontend] Added cards section UI

--- Pages/About.jsx
+++ Pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import AboutTop1 from '@/AboutTop1'
+import AboutCards from '@/AboutCards'
 
 const About = () => {
   return (
     <div className=''>
       <Navbar/>
       <AboutTop1/>
+      <AboutCards/>
     </div>
   )
 }
