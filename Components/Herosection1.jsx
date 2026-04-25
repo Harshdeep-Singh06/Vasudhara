89ad04081e7d74c2a54acba95bfeb447c7350b8d
Ayush <ayushbahuguna006@gmail.com>
2026-04-25 17:00:39 +0530

    [frontend] UX updation

--- Components/Herosection1.jsx
+++ Components/Herosection1.jsx
@@ -43,6 +43,7 @@ function Herosection1() {
         muted
         playsInline
         className='absolute top-0 left-0 w-full h-full object-cover'
+        poster="/hero-poster.jpg"
       >
         <source src='/herovideo.mp4' type='video/mp4' />
       </video>
